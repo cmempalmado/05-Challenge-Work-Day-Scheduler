a6d27e7549d96d769e95a32b7b20c45776913eed
cmempalmado <cmempalmado@gmail.com>
2022-09-28 02:55:47 -0700

    finished coding plus pictures addded

--- script.js
+++ script.js
@@ -1,66 +1,41 @@
-/**
- * DEFINE VARIABLES
- */
+currentDay = $("#currentDay")
+    .text(moment().format("dddd, MMMM D, YYYY"));
 
- var saveBtn = $(".saveBtn");
+function getCurrentClass(rowHour) {
+    var currentTime = moment();
+    var rowTime = moment(currentTime);
+    
+    rowTime.set({hour: rowHour});
 
- /**
-  * FUNCTIONS
-  */
- 
- // current day is displayed at the top of the calendar
- $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
- 
- // each time block is color-coded to indicate whether it is in the past, present, or future
- function timeBlockColor() {
-     var hour = moment().hours();
- 
-     $(".time-block").each(function() {
-         var currHour = parseInt($(this).attr("id"));
- 
-         // console.log(this); //each time-block
- 
-         if (currHour > hour) {
-             $(this).addClass("future");
-         } else if (currHour === hour) {
-             $(this).addClass("present");
-         } else {
-             $(this).addClass("past");
-         }
-     })
- };
- 
- // WHEN I click the save button for that time block
- saveBtn.on("click", function() {
- 
-     // console.log(this); //save button
-     var time = $(this).siblings(".hour").text();
-     var plan = $(this).siblings(".plan").val();
- 
-     // THEN the text for that event is saved in local storage
-     localStorage.setItem(time, plan);
- });
- 
- // WHEN I refresh the page
- // THEN the saved events persist
- function usePlanner() {
- 
-     $(".hour").each(function() {
-         var currHour = $(this).text();
-         var currPlan = localStorage.getItem(currHour);
- 
-         // console.log(this);
-         // console.log(currHour);
- 
-         if(currPlan !== null) {
-             $(this).siblings(".plan").val(currPlan);
-         }
-     });
- }
- 
- /**
-  * CALL FUNCTIONS
-  */
- 
- timeBlockColor();
- usePlanner();
+    if (currentTime.isSame(rowTime, "hour")) {
+        return "present";
+    }
+    else if (currentTime.isAfter(rowTime, "hour")) {
+        return "past";
+    }
+    else {
+        return "future";
+    }
+}
+
+function setColour(hour) {
+    var row = $("#" + hour);
+    var className = getCurrentClass(hour);
+    row.attr("class", "col-9 time-block " + className);
+}
+
+var hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+for (var i = 0; i < hours.length; i++) {
+    setColour(hours[i]);
+    loadBlock(hours[i]);
+}
+
+function saveBlock(id) {
+    var blockContent = $("#" + id).val();
+    localStorage.setItem("blockContent" + id, blockContent);
+}
+
+function loadBlock(id) {
+    var blockContent = localStorage.getItem("blockContent" + id);
+    $("#" + id).val(blockContent);
+}
